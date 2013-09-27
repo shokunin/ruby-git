5d15e1825bebbfdbcf4e6ffa99ee32a3db482d0a
Chris Mague <github@mague.com>
2013-09-27 14:18:52 -0700

    need branches in the run part

--- lib/git/log.rb
+++ lib/git/log.rb
@@ -125,7 +125,8 @@ module Git
       def run_log      
         log = @base.lib.full_log_commits(:count => @count, :object => @object, 
                                     :path_limiter => @path, :since => @since, 
-                                    :author => @author, :grep => @grep, :skip => @skip,
+                                    :author => @author, :grep => @grep, 
+                                    :skip => @skip, :branches => @branches,
                                     :until => @until, :between => @between)
         @commits = log.map { |c| Git::Object::Commit.new(@base, c['sha'], c) }
       end
